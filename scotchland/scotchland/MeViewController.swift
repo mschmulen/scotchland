49b4d943856088b9dcd6329938324fb7ea64ca35
matt.schmulen@elementum.com <matt.schmulen@elementum.com>
2014-11-21 14:53:27 -0800

    first

---
+++ scotchland/scotchland/MeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  scotchland
+//
+//  Created by Matt Schmulen on 11/21/14.
+//  Copyright (c) 2014 Scotchland. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
